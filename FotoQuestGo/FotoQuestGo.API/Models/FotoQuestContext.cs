aed882af81ef14bd8a8712602835c95c072b5106
Joan DIMKO <j.dimko@iaea.org>
2019-03-25 15:54:55 +0100

    Added EF Sql Server DBContext + connection string

---
+++ FotoQuestGo/FotoQuestGo.API/Models/FotoQuestContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FotoQuestGo.API.Models
+{
+    public class FotoQuestContext : DbContext
+    {
+        public FotoQuestContext(DbContextOptions<FotoQuestContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Quest> Quests { get; set; }
+    }
+}
