a8ab96e0ac1e1e74353412412672acde0432654f
Joan DIMKO <J.Dimko@iaea.org>
2019-03-28 16:12:44 +0100

    test FotoQuestGo.API.Foto.IntegrationTesting CanUploadFile passing through :-)

--- FotoQuestGo/FotoQuestGo.API.Foto/Startup.cs
+++ FotoQuestGo/FotoQuestGo.API.Foto/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace FotoQuestGo.API.Foto
 {
@@ -20,7 +21,7 @@ namespace FotoQuestGo.API.Foto
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-            services.AddScoped<IFotoStorage, FotoStorage>();
+            services.TryAddScoped<IFotoStorage, FotoStorage>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
